55681416e8e76e6dbca715439b8f17ad71eeffac
Lee Acevedo <91152882+Zirodev1@users.noreply.github.com>
2024-08-06 19:41:35 -0500

    added images, header, Image slider

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageSlider from "../components/ImageSlider";
+
+const Home = () => {
+    return (
+        <div>
+            <ImageSlider/>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Home;
